b3e2a6f630418bcd25ce11cec885bb085418f069
Masatoshi Hayashi <m@literalice.com>
2021-08-22 12:56:55 +0900

    creates a sample app

---
+++ app/app.py
@@ -0,0 +1,15 @@
+import signal
+import time
+
+run = True
+
+def handler_stop_signals(signum, frame):
+    global run
+    run = False
+
+signal.signal(signal.SIGINT, handler_stop_signals)
+signal.signal(signal.SIGTERM, handler_stop_signals)
+
+while run:
+    print('Hello \n')
+    time.sleep(5)
